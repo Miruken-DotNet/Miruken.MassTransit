c4f92fc88a60b2d1263217bde5e92546b7642aa4
Michael Dudley <michael.dudley@improving.com>
2019-05-01 15:40:51 -0500

    Empty solution and projects

---
+++ Source/Miruken.MassTransit/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Miruken.MassTransit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Miruken.MassTransit")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef5a1aa3-66fb-499a-952b-852bfe774d41")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
---
+++ Test/UnitTests/TestConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestConfiguration
+    {
+        [TestMethod]
+        public void IsRunning()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
