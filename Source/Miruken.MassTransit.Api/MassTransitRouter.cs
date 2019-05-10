b4ac76117289b4198d84bf426dad2fa4a1637218
Michael Dudley <michael.dudley@improving.com>
2019-05-10 12:22:42 -0500

    trying to debug

--- Source/Miruken.MassTransit.Api/MassTransitRouter.cs
+++ Source/Miruken.MassTransit.Api/MassTransitRouter.cs
@@ -24,9 +24,17 @@ namespace Miruken.MassTransit.Api
             var uri = new Uri(routed.Route);
             if (routed.Message.GetType().IsClassOf(typeof(IRequest<>)))
             {
-                var client = _bus.CreateRequestClient<Request, Response>(uri, TimeSpan.FromSeconds(30));
-                var result = await client.Request(new Request(routed.Message));
-                return result.Payload;
+                try
+                {
+                    var client = _bus.CreateRequestClient<Request, Response>(uri, TimeSpan.FromSeconds(30));
+                    var result = await client.Request(new Request(routed.Message));
+                    return result.Payload;
+                }
+                catch (Exception e)
+                {
+                    var a = 1;
+                    throw;
+                }
             }
 
             if (command.Many)
